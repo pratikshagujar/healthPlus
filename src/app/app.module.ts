7e4da6ee8aa8787520f3805859fe969c25c60614
pratikshagujar <pratikshagujar59@gmail.com>
2023-03-16 16:18:52 +0530

    medicine implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MedicineHomeComponent } from './components/medicine-home/medicine-home.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopDealsComponent } from './components/top-deals/top-deals.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MedicineHomeComponent,
+    TopDealsComponent
+ 
+ 
+ 
   ],
   imports: [
+    AppRoutingModule,
+    CoreModule,
+    CarouselModule,
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/medicine-home/medicine-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicine-home',
+  templateUrl: './medicine-home.component.html',
+  styleUrls: ['./medicine-home.component.scss']
+})
+export class MedicineHomeComponent {
+
+
+
+ 
+}
